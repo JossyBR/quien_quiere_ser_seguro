7cb71e0e1d0e245f0a3e4bc98ef55ab79727a1f0
Jossy Benitez <jossybr94@gmail.com>
2024-02-07 20:52:26 -0500

    Trabajando en estilos mobile

--- resources/js/Pages/Preguntas/Preguntas.jsx
+++ resources/js/Pages/Preguntas/Preguntas.jsx
@@ -120,8 +120,8 @@ const Preguntas = ({
     if (!preguntaActual) return <div>Cargando...</div>;
 
     return (
-        <div className="bg-[#010326] opacity-[0.9] text-white min-h-screen p-8">
-            <h1 className="text-4xl font-bold mb-4">
+        <div className="bg-[#0A2342] text-white min-h-screen p-8">
+            <h1 className="text-xl text-center md:text-4xl font-bold mb-4">
                 ¿QUIEN QUIERE SER SEGURO?
             </h1>
             <div className="mb-4">
@@ -134,38 +134,60 @@ const Preguntas = ({
                     Ver preguntas{" "}
                 </Link>
             </div>
-            <button
-                onClick={reiniciarJuego}
-                className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded mr-2"
-            >
-                Reiniciar juego
-            </button>{" "}
+            <div className="border flex justify-between">
+                <div>
+                    <button
+                        onClick={reiniciarJuego}
+                        className=" text-white font-bold py-2 px-4 rounded mr-2"
+                    >
+                        Reiniciar juego
+                    </button>
+                </div>
+
+                <div>
+                    <button
+                        onClick={ayudaCincuenta}
+                        className="border-2 rounded-full text-white font-bold p-2 "
+                    >
+                        50/50
+                    </button>
+                </div>
+            </div>{" "}
             <br />
-            <button
-                onClick={ayudaCincuenta}
-                className="bg-yellow-500 hover:bg-yellow-700 text-white font-bold py-2 px-4 rounded"
-            >
-                50/50
-            </button>
-            <div id="temporizador" className="mt-4 mb-4 text-2xl">
-                Tiempo restante:{" "}
-                <span className="font-bold">{tiempoRestante}</span> segundos
+            <div className="border-2">
+                <div
+                    id="temporizador"
+                    className="text-base text-center mb-4 lg:text-2xl"
+                >
+                    Tiempo restante:{" "}
+                    <span className="font-bold">{tiempoRestante}</span> segundos
+                </div>
+                <div className="flex justify-center gap-2 ">
+                    <button onClick={iniciarTemporizador}>Iniciar</button>
+                    <button onClick={detenerTemporizador}>Detener</button>
+                </div>
             </div>
-            <button onClick={iniciarTemporizador}>Iniciar Temporizador</button>
-            <button onClick={detenerTemporizador}>Detener Temporizador</button>
-            <div className={styles.div}>hola soy de prueba</div>
-            <div className={styles.hexagon}>
-                <h2 className="text-3xl font-bold mb-4">
-                    {preguntaActual.pregunta}
-                </h2>
+            <h1 className="text-xl text-center md:text-4xl font-bold mb-4">
+                ¿LOGO?
+            </h1>
+            <p className="text-base">
+                Puntaje actual:{" "}
+                <span className="font-bold">{puntajeLocal}</span>
+            </p>
+            <div className="flex justify-center">
+                <div className={styles.hexagon}>
+                    <h2 className="text-lg font-bold mb-4 text-center">
+                        {preguntaActual.pregunta}
+                    </h2>
+                </div>
             </div>
-            <div className="grid grid-cols-1 md:grid-cols-2 gap-4 border">
+            <div className={`flex flex-col border-2 border-black`}>
                 {respuestasFiltradas.length > 0
                     ? respuestasFiltradas.map((respuesta, index) => (
                           <button
                               id={`respuesta-btn-${index}`}
                               key={index}
-                              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 border"
+                              className={`border-2`}
                               onClick={() =>
                                   verificarRespuesta(respuesta, index)
                               }
@@ -182,7 +204,7 @@ const Preguntas = ({
                           <button
                               id={`respuesta-btn-${index}`}
                               key={index}
-                              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+                              className={`border-2 ${styles.hexagon}`}
                               onClick={() =>
                                   verificarRespuesta(respuesta, index)
                               }
@@ -191,16 +213,12 @@ const Preguntas = ({
                           </button>
                       ))}
             </div>
-            <p className="text-xl">
-                Puntaje actual:{" "}
-                <span className="font-bold">{puntajeLocal}</span>
-            </p>
-            <div>
+            <div className="flex justify-center mt-2">
                 {/* Si indiceactual es menor que totalpreguntas quiere decir que hay mas preguntas despues de la actual se resta 1 de $totalPreguntas para ajustar el hecho de que los índices comienzan en 0 */}
                 {indiceActual < totalPreguntas - 1 && (
                     <button
                         onClick={irASiguientePregunta}
-                        className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded mr-2"
+                        className="border text-white font-bold py-2 px-4 rounded mr-2"
                     >
                         Continuar
                     </button>
@@ -209,7 +227,7 @@ const Preguntas = ({
                 {indiceActual > 0 && (
                     <button
                         onClick={irAPreguntaAnterior}
-                        className="bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded"
+                        className="border text-white font-bold py-2 px-4 rounded"
                     >
                         Anterior
                     </button>
